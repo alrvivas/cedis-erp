4cedbca13063b3a310547d2f501e062c273f357a
alrvivas <alr.vivas@gmail.com>
2018-09-05 17:10:27 -0500

    add-persons

--- campoapp/address/models.py
+++ campoapp/address/models.py
@@ -14,8 +14,8 @@ class Municipality(models.Model):
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     name = models.CharField(max_length=140)
 
-    def __str__(self):
-        return self.name
+    def __unicode__(self):
+        return unicode(self.name)
 
 
 class Location(models.Model):
