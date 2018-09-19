c4e2c874e23e538ad8f28a3c4b7e22f38dfba38d
alrvivas <alr.vivas@gmail.com>
2018-09-19 17:51:58 -0500

    client

--- campoapp/address/urls.py
+++ campoapp/address/urls.py
@@ -3,12 +3,10 @@ from django.urls import path,re_path
 
 from .views import (
     AutocomplateView,
-    AutocomplateStateView
 )
 app_name = 'address'
 urlpatterns = [
 
-	re_path(r'^autocomplate-state/$', AutocomplateStateView.as_view(), name='autocomplate_state'),
     re_path(r'^autocomplate-location/$', AutocomplateView.as_view(), name='autocomplate_location'),
 
 ]
